84d24c06c19de1ca66c71cec9c87f4248ff8b747
koekeishiya <aasvi93@hotmail.com>
2015-09-11 23:49:14 +0200

    changed code style

--- tictactoe.java
+++ tictactoe.java
@@ -1,79 +1,88 @@
 import java.net.*;
 import java.io.*;
 
-public class tictactoe {
-      public static void main(String[] args) throws Exception {
-            BufferedReader localInput = new BufferedReader(new InputStreamReader(System.in));
-            boolean isListener = false;
+public class tictactoe 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        BufferedReader localInput = new BufferedReader(new InputStreamReader(System.in));
+        boolean isListener = false;
 
-            System.out.print("Enter server ip (leave blank to host): ");
-            String host = localInput.readLine();
+        System.out.print("Enter server ip (leave blank to host): ");
+        String host = localInput.readLine();
 
-            if(host.equals("")) {
-                  // i am host
-                  isListener = true;
+        // i am host
+        if(host.equals("")) 
+            isListener = true;
+
+        int port = 1337;
+        boolean canPlay = false;
+
+        // initialize server and client to null
+        tttServer server = null;
+        tttClient client = null;
+
+        if(isListener) 
+        {
+            // is host
+            server = new tttServer(port);
+            if(server.hasConnection()) 
+            {
+                // if a client has connected, can start game
+                canPlay = true;
+            } else 
+            {
+                // client connection failed, abort
+                System.out.println("Connection to peer failed!");
+                return;
             }
+        } else 
+        {
+            // is client, has connected to server
+            client = new tttClient(host, port);
+            canPlay = true;
+        }
 
-            int port = 1337;
-            boolean canPlay = false;
+        if(canPlay) 
+        {
+            // am i using client or server socket
+            Socket gameSocket = (isListener ? server.connection() : client.connection());
 
-            // initialize server and client to null
-            tttServer server = null;
-            tttClient client = null;
+            // initialize new game with the correct socket and host status
+            tttGame game = new tttGame(gameSocket, isListener);
 
-            if(isListener) {
-                  // is host
-                  server = new tttServer(port);
-                  if(server.hasConnection()) {
-                        // if a client has connected, can start game
-                        canPlay = true;
-                  } else {
-                        // client connection failed, abort
-                        System.out.println("Connection to peer failed!");
-                        return;
-                  }
-            } else {
-                  // is client, has connected to server
-                  client = new tttClient(host, port);
-                  canPlay = true;
+            // while game is in progress, and no winner is chosen
+            while(game.isRunning()) 
+            {
+                // if my turn, ask for a valid move
+                if(game.myTurn()) 
+                {
+                    boolean res = false;
+                    do 
+                    {
+                        System.out.print("Enter your move (x y): ");
+                        String move[] = localInput.readLine().split(" ");
+                        int x = Integer.parseInt(move[0]);
+                        int y = Integer.parseInt(move[1]);
+                        res = game.performMove(x,y);
+                    } while(!res);
+                    // a valid move has been performed, wait for response
+                    System.out.println("Waiting for opponent...");
+                }
+
+                // not my turn, waiting for clients move
+                else if(!game.myTurn()) 
+                {
+                    String move[] = game.readMessage().split(" ");
+                    int x = Integer.parseInt(move[0]);
+                    int y = Integer.parseInt(move[1]);
+                    game.performMove(x,y);
+                }
+
+                // has any player won?
+                game.checkWinner();
             }
-
-            if(canPlay) {
-                  // am i using client or server socket
-                  Socket gameSocket = (isListener ? server.connection() : client.connection());
-
-                  // initialize new game with the correct socket and host status
-                  tttGame game = new tttGame(gameSocket, isListener);
-
-                  // while game is in progress, and no winner is chosen
-                  while(game.isRunning()) {
-
-                        // if my turn, ask for a valid move
-                        if(game.myTurn()) {
-                              boolean res = false;
-                              do {
-                                    System.out.print("Enter your move (x y): ");
-                                    String move[] = localInput.readLine().split(" ");
-                                    int x = Integer.parseInt(move[0]);
-                                    int y = Integer.parseInt(move[1]);
-                                    res = game.performMove(x,y);
-                              } while(!res);
-                              // a valid move has been performed, wait for response
-                              System.out.println("Waiting for opponent...");
-                        }
-
-                        // not my turn, waiting for clients move
-                        else if(!game.myTurn()) {
-                              String move[] = game.readMessage().split(" ");
-                              int x = Integer.parseInt(move[0]);
-                              int y = Integer.parseInt(move[1]);
-                              game.performMove(x,y);
-                        }
-
-                        // has any player won?
-                        game.checkWinner();
-                  }
-                  game.exit();
-            }
-      }
+            game.exit();
+        }
+    }
 }
--- tttServer.java
+++ tttServer.java
@@ -1,29 +1,34 @@
 import java.net.*;
 import java.io.*;
 
-public class tttServer {
-      private ServerSocket listener;
-      private Socket connection;
-      private int port;
-      private boolean hasConnection;
+public class tttServer 
+{
+    private ServerSocket listener;
+    private Socket connection;
+    private int port;
+    private boolean hasConnection;
 
-      public tttServer(int p) throws Exception {
-            this.port = p;
-            this.hasConnection = false;
+    public tttServer(int p) throws Exception 
+    {
+        this.port = p;
+        this.hasConnection = false;
 
-            System.out.println("Listening on port " + this.port + "...");
-            listener = new ServerSocket(port);
-            while (!this.hasConnection) {
-                        connection = listener.accept();
-                        this.hasConnection = true;
-            }
-      }
+        System.out.println("Listening on port " + this.port + "...");
+        listener = new ServerSocket(port);
+        while (!this.hasConnection) 
+        {
+            connection = listener.accept();
+            this.hasConnection = true;
+        }
+    }
 
-      public boolean hasConnection() {
-            return hasConnection;
-      }
+    public boolean hasConnection() 
+    {
+        return hasConnection;
+    }
 
-      public Socket connection() {
-            return connection;
-      }
+    public Socket connection() 
+    {
+        return connection;
+    }
 }
